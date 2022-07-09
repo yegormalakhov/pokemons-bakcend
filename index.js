f1a5a15b3d64f176addca5e72776e8170efafb65
ymalakhov <it.malakhov@gmail.com>
2022-07-09 15:26:39 +0200

    starter code
    Co-authored-by: Eike Merx <eikemx@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+var cors = require("cors");
+// const pokemonsDB = require("./dbs/pokemonsDB");
+const pokemonRoute = require("./routes/pokemons");
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+app.use("/pokemons", pokemonRoute);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
---
+++ routes/pokemons.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const pokemonRoute = express.Router();
+const pokemonsDB = require("../dbs/pokemonsDB");
+
+const pokemons = pokemonsDB.pokemons;
+// console.log(pokemons);
+pokemonRoute.get("/", (req, res) => {
+  res.send(pokemons);
+});
+
+pokemonRoute.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const targetPokemon = pokemons.find((pokemon) => pokemon.id == id); //Need to change string number to number
+  console.log(targetPokemon);
+  res.send(targetPokemon);
+});
+
+module.exports = pokemonRoute;

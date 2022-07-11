8053b536bba3c0f931c3cbe722cc6aaa1e161bc4
ymalakhov <it.malakhov@gmail.com>
2022-07-11 17:17:26 +0200

    add sttus code to res, change .json to send in pokemons / route Co-authored-by: Eike Merx <eikemx@users.noreply.github.com>

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 var cors = require("cors");
-// const pokemonsDB = require("./dbs/pokemonsDB");
 const pokemonRoute = require("./routes/pokemons");
 app.use(express.json());
+app.use(express.static("public"));
 app.use(cors());
 
 const PORT = process.env.PORT || 3008;
--- routes/pokemons.js
+++ routes/pokemons.js
@@ -5,14 +5,14 @@ const pokemonsDB = require("../dbs/pokemonsDB");
 const pokemons = pokemonsDB.pokemons;
 // console.log(pokemons);
 pokemonRoute.get("/", (req, res) => {
-  res.send(pokemons);
+  res.status(500).send(pokemons);
 });
 
 pokemonRoute.get("/:id", (req, res) => {
   const { id } = req.params;
   const targetPokemon = pokemons.find((pokemon) => pokemon.id == id); //Need to change string number to number
   console.log(targetPokemon);
-  res.send(targetPokemon);
+  res.status(500).send(targetPokemon);
 });
 
 module.exports = pokemonRoute;

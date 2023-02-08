22094606880683967d51cf7b260d8d1f85b33c53
ymalakhov <it.malakhov@gmail.com>
2023-02-08 21:27:53 +0100

    res status change

--- routes/pokemons.js
+++ routes/pokemons.js
@@ -5,14 +5,14 @@ const pokemonsDB = require("../dbs/pokemonsDB");
 const pokemons = pokemonsDB.pokemons;
 // console.log(pokemons);
 pokemonRoute.get("/", (req, res) => {
-  res.status(500).send(pokemons);
+  res.send(pokemons);
 });
 
 pokemonRoute.get("/:id", (req, res) => {
   const { id } = req.params;
-  const targetPokemon = pokemons.find((pokemon) => pokemon.id == id); //Need to change string number to number
+  const targetPokemon = pokemons.find((pokemon) => pokemon.id == Number(id)); //Need to change string number to number
   console.log(targetPokemon);
-  res.status(500).send(targetPokemon);
+  res.send(targetPokemon);
 });
 
 module.exports = pokemonRoute;
